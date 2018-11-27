489026fe8b063153ee8d5a6629dff8e584776616
Chris Talman <cewe.wow@gmail.com>
2018-11-27 19:46:50 +0000

    Introduced type 'PromiseResolution'.

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,5 @@
-declare type Omit <GenericObject extends object, GenericOmissions extends string> = Pick<GenericObject, Exclude<keyof GenericObject, GenericOmissions>>
+/** Omits properties from object. */
+declare type Omit <GenericObject extends object, GenericOmissions extends string> = Pick<GenericObject, Exclude<keyof GenericObject, GenericOmissions>>;
+
+/** Returns resolution type of a promise. */
+declare type PromiseResolution <T> = T extends Promise<infer V> ? V : never;
