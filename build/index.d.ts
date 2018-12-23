75f2503d5dbb8382d2e073fcef4847a9dab44222
Chris Talman <cewe.wow@gmail.com>
2018-12-23 17:41:42 +0000

    Changed TypeScript config to output comments, which has the effect of including type comments in the output.

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,2 +1,4 @@
+/** Omits properties from object. */
 export declare type Omit<GenericObject extends object, GenericOmissions extends string> = Pick<GenericObject, Exclude<keyof GenericObject, GenericOmissions>>;
+/** Returns resolution type of a promise. */
 export declare type PromiseResolution<T> = T extends Promise<infer V> ? V : never;
