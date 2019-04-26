3a4a6c566c2660e4370e94fd5edbcd6db44c10b9
Chris Talman <cewe.mail@gmail.com>
2019-04-26 19:15:10 +0100

    Rebuilt index.d.ts with PartialDeep.

--- build/index.d.ts
+++ build/index.d.ts
@@ -2,3 +2,7 @@
 export declare type Omit<GenericObject extends object, GenericOmissions extends string> = Pick<GenericObject, Exclude<keyof GenericObject, GenericOmissions>>;
 /** Returns resolution type of a promise. */
 export declare type PromiseResolution<T> = T extends Promise<infer V> ? V : never;
+/** Makes all properties optional in interface and subinterfaces. */
+export declare type PartialDeep<T> = {
+    [P in keyof T]?: T[P] extends (infer U)[] ? PartialDeep<U>[] : T[P] extends object ? PartialDeep<T[P]> : T[P];
+};
